50a82e1df4817b62c0765c7c8dc7ab086a58278b
anon <anon@non>
2025-03-30 23:35:55 +0700

    no write

--- include/koalabox/io.hpp
+++ include/koalabox/io.hpp
@@ -5,16 +5,4 @@
 namespace koalabox::io {
 
     String read_file(const Path& file_path);
-
-    /**
-     * Write a string to file at the given path.
-     * @return `true` if operation was successful, `false` otherwise
-     */
-    bool write_file(const Path& file_path, const String& contents) noexcept;
-
-    bool unzip_file(
-        const Path& source_zip,
-        const String& target_file,
-        const Path& destination_dir
-    ) noexcept;
 }
